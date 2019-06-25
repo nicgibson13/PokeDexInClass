14b6902ebe2207ac87f3285240f43e200b61d8de
Nic Gibson <nic@shpangy.com>
2019-06-25 10:58:01 -0600

    Initial Commit

---
+++ PokeDexInClass/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PokeDexInClass
+//
+//  Created by Nic Gibson on 6/25/19.
+//  Copyright © 2019 Nic Gibson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
